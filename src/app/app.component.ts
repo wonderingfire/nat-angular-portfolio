4818bc661e3da6a2b9222f94fbb1869704a9a3b1
Nathaniel Chavez <nathanielchavez.codes@gmail.com>
2025-03-01 10:40:29 +0800

    test commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: 'app.component.html',
+  styleUrl: 'app.component.scss',
 })
 export class AppComponent {
   title = 'nat-angular-portfolio';
